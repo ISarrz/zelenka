34914096cf46a8e1379db56816744a4e9a57115f
German Zlobin <gezlobin@edu.hse.ru>
2025-12-09 16:31:55 +0300

    7

--- scripts/server.py
+++ scripts/server.py
@@ -1,43 +1,13 @@
 import socket
-import datetime as dt
-import pytz
-
+import time
 
 HOST = "0.0.0.0"
-PORT = 4600
-
-moscow_timezone = pytz.timezone('Europe/Moscow')
-
-with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-    s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-    s.bind((HOST, PORT))
-    s.listen()
-    print(f"Server listening on {HOST}:{PORT}")
-
-    while True:
-        try:
-            print("Waiting for connection...")
-            conn, addr = s.accept()
-            print("Connected by", addr)
-
-            with conn:
-                while True:
-                    data = conn.recv(1024)
-
-                    if not data:
-                        print(f"Connection closed by {addr}")
-                        break
-
-                    received_message = data.decode("utf-8").strip()
-                    print(f"Received from {addr}: {received_message}")
-
-
-                    date = dt.datetime.now(moscow_timezone).strftime("%d.%m.%Y %H:%M:%S")
-
-                    response_message = f"Hello, {date}"
-                    conn.sendall(response_message.encode("utf-8"))
-                    print(f"Sent response to {addr}: {response_message}")
-
-        except Exception as e:
-            print(f"General Error: {e}")
-            continue
+PORT = 1234
+server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+server_socket.bind((HOST, PORT))
+server_socket.listen(5)
+conn, addr = server_socket.accept()
+print(conn.recv(1024))
+conn.send(b"Hello, world!")
+conn.close()
